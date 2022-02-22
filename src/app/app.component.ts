05c3a1e2e093534a39229c1796bd64dfb1c2492f
Bakytai Egemberdieva <bakijw@gmail.com>
2022-02-22 17:32:18 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hw82';
+  coderObject: any;
+  numbers = [1,2,3,4,5,6,7,8,9,0];
+  code!: Observable<Object>;
+
+  constructor() {
+  }
+
+  onEnter() {
+
+  }
+
+  codeInsert(number: number) {
+
+  }
+
+  deleteLastSymbol() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { keypadReducer } from './keypad.reducer';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({code: keypadReducer}, {}),
   ],
   providers: [],
   bootstrap: [AppComponent]
