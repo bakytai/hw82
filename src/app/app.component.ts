1f59c476be3755928cd750b3580eb90af5cdc167
Bakytai Egemberdieva <bakijw@gmail.com>
2022-02-22 18:47:02 +0600

    вывожу звездочки вместо цифр

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { addChar, chek, deleteChar } from './keypad.actions';
+import { StateModel } from './state.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   numbers = [1,2,3,4,5,6,7,8,9,0];
   code!: Observable<Object>;
+  myState!: StateModel;
+  stateSubscription!: Subscription;
+  symbolsArr: string[] = [];
 
   constructor(private store: Store<{code: Object}>) {
     this.code = store.select('code');
+    this.stateSubscription =  this.code.subscribe( state => {
+      this.myState = <StateModel>state;
+      this.symbolsArr = this.myState.password.split('')
+    })
   }
 
   onEnter() {
     this.store.dispatch(chek());
+    if (this.myState.isCorrect) {
+
+    }
   }
 
-  codeInsert(numberValue: number) {
+  getNumber(numberValue: number) {
     this.store.dispatch(addChar({char: numberValue.toString()}));
   }
 
   deleteLastSymbol() {
     this.store.dispatch(deleteChar());
+  }
+
+  ngOnDestroy() {
+    this.stateSubscription.unsubscribe();
   }
 }
---
+++ src/app/state.model.ts
@@ -0,0 +1,7 @@
+export class StateModel {
+  constructor(
+    public isCorrect: boolean,
+    public password: string,
+  ) {
+  }
+}
