8a110609b57a14f800a2c53cf4446c675b6de12d
Bakytai Egemberdieva <bakijw@gmail.com>
2022-02-22 18:02:41 +0600

    прописала action на проверку пароля

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { addChar } from './keypad.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +9,19 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hw82';
-  coderObject: any;
   numbers = [1,2,3,4,5,6,7,8,9,0];
   code!: Observable<Object>;
 
-  constructor() {
+  constructor(private store: Store<{code: Object}>) {
+    this.code = store.select('code');
   }
 
   onEnter() {
 
   }
 
-  codeInsert(number: number) {
-
+  codeInsert(numberValue: number) {
+    this.store.dispatch(addChar({char: numberValue.toString()}));
   }
 
   deleteLastSymbol() {
--- src/app/keypad.actions.ts
+++ src/app/keypad.actions.ts
@@ -3,3 +3,7 @@ import { createAction, props } from '@ngrx/store';
 export const addChar = createAction(
   '[Keypad] Add character',
   props<{char: string}>());
+
+export const chek = createAction('[Keypad] Chek character');
+
+
